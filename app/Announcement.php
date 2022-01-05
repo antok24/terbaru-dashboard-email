59256b385a70fdf5d1e0550e3d587a911587e611
antok24 <antokbojonegoro@gmail.com>
2022-01-05 21:53:49 +0700

    first commit

---
+++ app/Announcement.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Announcement extends Model
+{
+    protected $fillable = [
+        'subject',
+        'content',
+        'file',
+        'sender_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'id', 'sender_id');
+    }
+
+    public function RecipientAnnouncement()
+    {
+        return $this->hasMany('App\RecipientAnnouncement', 'announcement_id', 'id');
+    }
+}
